38aab6abfde2847be0ba9871ddd8f749d7473e80
sazid.ali@softobiz.com <sazid.ali@softobiz.com>
2022-02-14 09:13:40 +0530

    first commit

--- Server.js
+++ Server.js
@@ -1,19 +1,16 @@
 const http = require('http');
-const express= require('express')
-const app=express()
 const hostname = '127.0.0.1';
 const port = 3003;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`server is running at ${port}`);
+  if(res.statusCode = 200)
+  {res.setHeader('Content-Type', 'text/plain');
+  res.end(`server is running at ${port}`);}
+  else{
+    res.end('Something Went Wrong')
+  }
 });
 
-app.get('/Cars',(req, res)=>{
-  res.end('cars data')
-  console.log("hello")
-})
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
